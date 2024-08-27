4d38098e7990fb0b1d6cb16e0e12b77a65a49b64
swgamez <wilgamez@hotmail.com>
2024-08-27 09:38:55 -0700

    Update batocera_install_gpi2.sh

--- batocera_install_gpi2.sh
+++ batocera_install_gpi2.sh
@@ -8,14 +8,44 @@ sleep 2s
 mount -o remount, rw /boot
 mount -o remount, rw /
 
-#Download Python script-----------------------------
+#RetroFlag pw io ;2:in ;3:in ;4:in ;14:out 1----------------------------------------
+File=/boot/config.txt
+wget -O  "/boot/overlays/RetroFlag_pw_io.dtbo" "$SourcePath/RetroFlag_pw_io.dtbo"
+if grep -q "RetroFlag_pw_io" "$File";
+	then
+		sed -i '/RetroFlag_pw_io/c dtoverlay=RetroFlag_pw_io.dtbo' $File 
+		echo "PW IO fix."
+	else
+		echo "dtoverlay=RetroFlag_pw_io.dtbo" >> $File
+		echo "PW IO enabled."
+fi
+if grep -q "enable_uart" "$File";
+	then
+		sed -i '/enable_uart/c enable_uart=1' $File 
+		echo "UART fix."
+	else
+		echo "enable_uart=1" >> $File
+		echo "UART enabled."
+fi
+
+#-----------------------------------------------------------
+
+sleep 2s
+
+if grep -q "system.power.switch" "/userdata/system/batocera.conf";
+	then
+		sed -i '/system.power.switch/c system.power.switch=RETROFLAG' "/userdata/system/batocera.conf"
+		echo "power switch fix."
+	else
+		echo "system.power.switch=RETROFLAG" >> /userdata/system/batocera.conf
+		echo "power switch enabled."
+fi
+#-----------------------------------------------------------
+
 mkdir /userdata/RetroFlag
 sleep 2s
 script=/userdata/RetroFlag/SafeShutdown.py
-
-wget -O  $script "$SourcePath/batocera_SafeShutdown_gpi2.py"
-wget -O  "/userdata/RetroFlag/lcdfirst.sh" "$SourcePath/batoceralcdfirst.sh"
-wget -O  "/userdata/RetroFlag/lcdnext.sh" "$SourcePath/batoceralcdnext.sh"
+wget -O  $script "$SourcePath/batocera_SafeShutdown.py"
 #-----------------------------------------------------------
 
 sleep 2s
@@ -30,12 +60,13 @@ if grep -q "python $script &" "$DIR";
 				chmod +x $DIR
 		fi
 	else
-		echo "python $script & sh /userdata/RetroFlag/lcdfirst.sh" >> $DIR
+		echo "python $script &" >> $DIR
 		chmod +x $DIR
-		chmod +x /userdata/RetroFlag/lcdfirst.sh
-		chmod +x /userdata/RetroFlag/lcdnext.sh
 		echo "Executable script configured."
 fi
 #-----------------------------------------------------------
 
-
+echo "RetroFlag Pi Case Switch installation done. Will now reboot after 3 seconds."
+sleep 3
+shutdown -r now
+#-----------------------------------------------------------
