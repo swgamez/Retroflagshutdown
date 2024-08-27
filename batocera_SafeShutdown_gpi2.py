ee40da5525112776fcb36aac3baa4601a718eba1
swgamez <wilgamez@hotmail.com>
2024-08-27 12:34:34 -0700

    Update batocera_SafeShutdown_gpi2.py

--- batocera_SafeShutdown_gpi2.py
+++ batocera_SafeShutdown_gpi2.py
@@ -5,11 +5,15 @@ import RPi.GPIO as GPIO
 # initialize pins
 
 powerPin = 26
+powerenPin = 27 
 
 #initialize GPIO settings
 def init():
 	GPIO.setmode(GPIO)
+	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(powerenPin, GPIO.OUT, initial=GPIO.HIGH)
+	GPIO.output(powerenPin, GPIO.HIGH)
 	GPIO.setwarnings(False)
 
 #waits for user to hold button up to 1 second before issuing poweroff command
