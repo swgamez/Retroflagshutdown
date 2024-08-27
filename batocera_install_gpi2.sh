ee402fbd337d87c07788365014b0cc51a9a47cea
swgamez <wilgamez@hotmail.com>
2024-08-27 09:46:22 -0700

    Update batocera_install_gpi2.sh

--- batocera_install_gpi2.sh
+++ batocera_install_gpi2.sh
@@ -8,38 +8,6 @@ sleep 2s
 mount -o remount, rw /boot
 mount -o remount, rw /
 
-#RetroFlag pw io ;2:in ;3:in ;4:in ;14:out 1----------------------------------------
-File=/boot/config.txt
-wget -O  "/boot/overlays/RetroFlag_pw_io.dtbo" "$SourcePath/RetroFlag_pw_io.dtbo"
-if grep -q "RetroFlag_pw_io" "$File";
-	then
-		sed -i '/RetroFlag_pw_io/c dtoverlay=RetroFlag_pw_io.dtbo' $File 
-		echo "PW IO fix."
-	else
-		echo "dtoverlay=RetroFlag_pw_io.dtbo" >> $File
-		echo "PW IO enabled."
-fi
-if grep -q "enable_uart" "$File";
-	then
-		sed -i '/enable_uart/c enable_uart=1' $File 
-		echo "UART fix."
-	else
-		echo "enable_uart=1" >> $File
-		echo "UART enabled."
-fi
-
-#-----------------------------------------------------------
-
-sleep 2s
-
-if grep -q "system.power.switch" "/userdata/system/batocera.conf";
-	then
-		sed -i '/system.power.switch/c system.power.switch=RETROFLAG' "/userdata/system/batocera.conf"
-		echo "power switch fix."
-	else
-		echo "system.power.switch=RETROFLAG" >> /userdata/system/batocera.conf
-		echo "power switch enabled."
-fi
 #-----------------------------------------------------------
 
 mkdir /userdata/RetroFlag
