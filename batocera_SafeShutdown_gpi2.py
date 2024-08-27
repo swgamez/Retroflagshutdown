fd6d2b07b1fca7eb1ddeb0ba091b33410bfed924
swgamez <wilgamez@hotmail.com>
2024-08-27 09:36:54 -0700

    Update batocera_SafeShutdown_gpi2.py

--- batocera_SafeShutdown_gpi2.py
+++ batocera_SafeShutdown_gpi2.py
@@ -8,30 +8,68 @@ powerPin = 26
 powerenPin = 27
 hdmiPin = 18
 
-# initialize GPIO settings
+#initialize GPIO settings
 def init():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(hdmiPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(powerenPin, GPIO.OUT)
-    GPIO.output(powerenPin, GPIO.HIGH)
-    GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(resetPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.HIGH)
+	GPIO.output(ledPin, GPIO.HIGH)
+	GPIO.setup(powerenPin, GPIO.OUT, initial=GPIO.HIGH)
+	GPIO.output(powerenPin, GPIO.HIGH)
+	GPIO.setwarnings(False)
 
-
-# waits for user to hold button up to 1 second before issuing poweroff command
+#waits for user to hold button up to 1 second before issuing poweroff command
 def poweroff():
-    while True:
-        GPIO.wait_for_edge(powerPin, GPIO.FALLING)
-        os.system("shutdown -h now")
+	while True:
+		#self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
+		#GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+		start = time.time()
+		while GPIO.input(powerPin) == GPIO.HIGH:
+			time.sleep(0.5)
+		os.system("batocera-es-swissknife --emukill")
+		os.system("shutdown -r now")
+
+#blinks the LED to signal button being pushed
+def ledBlink():
+	while True:
+		GPIO.output(ledPin, GPIO.HIGH)
+		#self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
+		#GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+		start = time.time()
+		while GPIO.input(powerPin) == GPIO.HIGH:
+			time.sleep(0.5)
+		while GPIO.input(powerPin) == GPIO.LOW:
+			GPIO.output(ledPin, GPIO.LOW)
+			time.sleep(0.2)
+			GPIO.output(ledPin, GPIO.HIGH)
+			time.sleep(0.2)
+
+#resets the pi
+def reset():
+	while True:
+		#self.assertEqual(GPIO.input(resetPin), GPIO.LOW)
+		#GPIO.wait_for_edge(resetPin, GPIO.FALLING)
+		start = time.time()
+		while GPIO.input(resetPin) == GPIO.HIGH:
+			time.sleep(0.5)
+		os.system("batocera-es-swissknife --emukill")
+		os.system("shutdown -r now")
 
 
 if __name__ == "__main__":
-    # initialize GPIO settings
-    init()
-    # create a multiprocessing.Process instance for each function to enable parallelism
-    powerProcess = Process(target=poweroff)
-    powerProcess.start()
+	#initialize GPIO settings
+	init()
+	#create a multiprocessing.Process instance for each function to enable parallelism 
+	powerProcess = Process(target = poweroff)
+	powerProcess.start()
+	ledProcess = Process(target = ledBlink)
+	ledProcess.start()
+	resetProcess = Process(target = reset)
+	resetProcess.start()
 
-    powerProcess.join()
+	powerProcess.join()
+	ledProcess.join()
+	resetProcess.join()
 
-    GPIO.cleanup()
+	GPIO.cleanup()
