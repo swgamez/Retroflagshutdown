37ca1181edf25c39c080d56e2a6d16394fc178f9
swgamez <wilgamez@hotmail.com>
2024-08-27 23:46:26 -0700

    Update batocera_SafeShutdown_gpi2.py

--- batocera_SafeShutdown_gpi2.py
+++ batocera_SafeShutdown_gpi2.py
@@ -17,11 +17,7 @@ def init():
 #waits for user to hold button up to 1 second before issuing poweroff command
 def poweroff():
 	while True:
-		#self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
 		GPIO.wait_for_edge(powerPin, GPIO.FALLING)
-		#start = time.time()
-		#while GPIO.input(powerPin) == GPIO.HIGH:
-		#	time.sleep(0.5)
 		os.system("batocera-es-swissknife --emukill")
 		time.sleep(1)
 		os.system("shutdown -h now")
