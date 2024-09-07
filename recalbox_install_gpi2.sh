31963d4f885b80013a196e01a934ca152c9347e0
swgamez <wilgamez@hotmail.com>
2024-09-07 00:35:23 -0700

    Update recalbox_install_gpi2.sh

--- recalbox_install_gpi2.sh
+++ recalbox_install_gpi2.sh
@@ -1,60 +1,52 @@
 #!/bin/bash
-make /boot writable---------------------------------
-sleep 2s
-filewebsite=https://raw.githubusercontent.com/swgamez/Retroflagshutdown/main
 
-mount -o remount, rw /boot
+# Step 1) Check if root ####################################
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+#-----------------------------------------------------------
+
+SourcePath=https://gitee.com/clock333mhz/GPiCase2-Script/raw/cn_main
+
+#-----------------------------------------------------------
+sleep 2s
+
+mount -o remount, rw /flash
 mount -o remount, rw /
 
-
-Download Python script-----------------------------
-mkdir /userdata/RetroFlag
+#Download Python script-----------------------------
+mkdir /storage/.RetroFlag/
 sleep 2s
+script=/storage/.RetroFlag/SafeShutdown.py
 
-script=/userdata/RetroFlag/SafeShutdown.py
-
-if [ -e $script ];
-	then
-		wget --no-check-certificate -O  $script "$filewebsite""/recalbox_SafeShutdown_gpi2.py"
-	else
-		wget --no-check-certificate -O  $script "$filewebsite""/recalbox_SafeShutdown_gpi2.py"
-fi
-wget --no-check-certificate -O  "/userdata/RetroFlag/lcdfirst.sh" "$filewebsite""/lcdfirst.sh"
-wget --no-check-certificate -O  "/userdata/RetroFlag/lcdnext.sh" "$filewebsite""/lcdnext.sh"
-#wget --no-check-certificate -O  "/userdata/RetroFlag/LCD.sh" "$filewebsite""/LCD.sh"
-
+wget -O  $script "$SourcePath/lakka_SafeShutdown_gpi2.py"
+wget -O  "/storage/.RetroFlag/lcdfirst.sh" "$SourcePath/lakkalcdfirst.sh"
+wget -O  "/storage/.RetroFlag/lcdnext.sh" "$SourcePath/lakkalcdnext.sh"
 #-----------------------------------------------------------
 
 sleep 2s
-
-Enable Python script to run on start up------------
-DIR=/etc/init.d/S99RetroFlag
-#sed '1,40 d' /etc/init.d/S99RetroFlag
-#		chmod +x /userdata/RetroFlag/lcdfirst.sh
-#		chmod +x /userdata/RetroFlag/lcdnext.sh
+# DIR=/userdata/system/custom.sh
+DIR=/storage/.config/autostart.sh
 
 if grep -q "python $script &" "$DIR";
 	then
-		if [ -x $DIR ];
+		if [ -x "$DIR" ];
 			then 
-				echo "Executable S99RetroFlag already configured. Doing nothing."
+				echo "Executable script already configured. Doing nothing."
 			else
 				chmod +x $DIR
 		fi
 	else
-		echo -e "python $script & \n sh /userdata/RetroFlag/lcdfirst.sh \n" >> $DIR 
-#		echo -e "python $script & \n/userdata/RetroFlag/LCD.sh \n" >> $DIR 
-#		echo "python $script &" >> $DIR
+		echo "python $script & sh /storage/.RetroFlag/lcdfirst.sh" >> $DIR
 		chmod +x $DIR
-#		chmod +x /userdata/RetroFlag/LCD.sh
-		chmod +x /userdata/RetroFlag/lcdfirst.sh
-		chmod +x /userdata/RetroFlag/lcdnext.sh
-		echo "Executable S99RetroFlag configured."
+		chmod +x /storage/.RetroFlag/lcdfirst.sh
+		chmod +x /storage/.RetroFlag/lcdnext.sh
+		echo "Executable script configured."
 fi
 #-----------------------------------------------------------
 
-#Step 5) Reboot to apply changes----------------------------
 echo "RetroFlag Pi Case Switch installation done. Will now reboot after 3 seconds."
 sleep 3
-reboot
+systemctl reboot
 #-----------------------------------------------------------
