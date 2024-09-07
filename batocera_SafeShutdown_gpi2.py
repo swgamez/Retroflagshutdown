75739550493ad0083700a69da04cf1119fbf54a0
swgamez <wilgamez@hotmail.com>
2024-09-06 23:56:36 -0700

    Update batocera_SafeShutdown_gpi2.py

--- batocera_SafeShutdown_gpi2.py
+++ batocera_SafeShutdown_gpi2.py
@@ -3,6 +3,12 @@ import os
 import time
 from multiprocessing import Process
 
+#initialize pins
+#powerPin = 3 #pin 5
+#ledPin = 14 #TXD
+#resetPin = 2 #pin 13
+#powerenPin = 4 #pin 5
+
 powerPin = 26 
 powerenPin = 27 
 
@@ -17,10 +23,19 @@ def init():
 #waits for user to hold button up to 1 second before issuing poweroff command
 def poweroff():
 	while True:
+		#self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
 		GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+		#start = time.time()
+		#while GPIO.input(powerPin) == GPIO.HIGH:
+		#	time.sleep(0.5)
 		os.system("batocera-es-swissknife --emukill")
-		time.sleep(0.1)
+		time.sleep(1)
 		os.system("shutdown -h now")
+
+def lcdrun():
+	while True:
+		os.system("sh /userdata/RetroFlag/lcdnext.sh")
+		time.sleep(1)
 
 if __name__ == "__main__":
 	#initialize GPIO settings
@@ -28,8 +43,10 @@ if __name__ == "__main__":
 	#create a multiprocessing.Process instance for each function to enable parallelism 
 	powerProcess = Process(target = poweroff)
 	powerProcess.start()
-
+	lcdrunProcess = Process(target = lcdrun)
+	lcdrunProcess.start()
 
 	powerProcess.join()
+	lcdrunProcess.join()
 
 	GPIO.cleanup()
